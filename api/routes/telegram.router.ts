f0a555abf1aa5812715594a6bc0547a295156c33
vitaly <vmstukalov@mail.ru>
2023-10-20 16:30:28 +0300

    Готово!

--- api/routes/telegram.router.ts
+++ api/routes/telegram.router.ts
@@ -1,12 +1,14 @@
 import express, { Request } from 'express';
 import {ITelegramUpdate} from "../interfaces/ITelegramUpdate";
 import {ask} from "../services/gpt.service";
+import {createPost} from "../services/zen.service";
 
 const telegramRouter = express.Router()
 
 telegramRouter.post("/webhook/:secret", async (req: Request<{ITelegramUpdate, secret: string}>, res) => {
 
     const secret = req.params.secret;
+    const isLocalhost = req.headers.host.includes("localhost")
 
     if (secret !== process.env.WEBHOOK) {
         res.status(403).json({ok: false, message: "Wrong Secret"})
@@ -17,7 +19,9 @@ telegramRouter.post("/webhook/:secret", async (req: Request<{ITelegramUpdate, se
 
     if (telegramUpdate.message) {
         const title = telegramUpdate.message.text
-        ask(title)
+        ask(title).then(text => {
+            createPost(title, text, isLocalhost)
+        })
     }
 
     res.json({ok: true});
